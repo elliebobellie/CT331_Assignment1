24a2c1fc1ed25ab3018b18284943025f5c19eecd
elliebobellie <23409382+elliebobellie@users.noreply.github.com>
2018-10-24 13:33:43 +0100

    1st commit - q1
    q1 finished

--- src/q1/assignment.c
+++ src/q1/assignment.c
@@ -1,6 +1,27 @@
-#include <stdio.h>
+// q1
 
-int main(int arg, char* argc[]){
-  printf("Hello assignment1.\n");
-  return 0;
+#include "pch.h"
+#include <iostream>
+
+int main(int arg, char* argc[])
+{
+	printf("Hello assignment1.\n");
+
+	double pi = 3.14;
+	char theLetterE = 'e';
+	char* pointerToE = &theLetterE;
+
+	int a = 2;
+	int* b = &a;
+	long c = 30500;
+	double* d = &pi;
+	char** e = &pointerToE;
+
+	printf("Size of a = %d\n", sizeof(a));
+	printf("Size of b = %d\n", sizeof(b));
+	printf("Size of c = %d\n", sizeof(c));
+	printf("Size of d = %d\n", sizeof(d));
+	printf("Size of e = %d\n", sizeof(e));
 }
+
+
